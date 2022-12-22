2e7500fc7e3e0ad16082195f8f6ce422987ffd71
Michael Dahl <md@commercer.de>
2022-12-22 17:39:25 +0100

    Feline icon support

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -22,6 +22,7 @@ return require('packer').startup(function(use)
   })
 
   use { 'feline-nvim/feline.nvim', branch = '0.5-compat' }
+  use('nvim-tree/nvim-web-devicons')
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -154,6 +154,11 @@ _G.packer_plugins = {
     path = "/home/michael/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
+  ["nvim-web-devicons"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    url = "https://github.com/nvim-tree/nvim-web-devicons"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/michael/.local/share/nvim/site/pack/packer/start/packer.nvim",
